61cb4f4bae11b64503c44d34cce4a0e5643c034c
yeziyue <huasifei@huasifei.com>
2026-08-11 18:09:11 +0800

    filogic.mk[wh5000]:添加qmodem_mwan模块编译

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1969,7 +1969,7 @@ define Device/huasifei_wh5000
   DEVICE_PACKAGES := kmod-hwmon-pwmfan kmod-usb3 mt7987-2p5g-phy-firmware \
 	kmod-mt7996e kmod-mt7992-23-firmware f2fsck mkf2fs luci-app-qmodem luci-light kmod-usb3 \
   luci-app-samba4 kmod-usb-storage kmod-usb-storage-uas ntfs-3g block-mount kmod-hwmon-pwmfan luci-app-fancontrol \
-  kmod-nft-offload kmod-nf-flow kmod-nf-conntrack kmod-nft-nat atenl
+  kmod-nft-offload kmod-nf-flow kmod-nf-conntrack kmod-nft-nat atenl luci-app-qmodem-mwan 
   KERNEL_LOADADDR := 0x40000000
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
